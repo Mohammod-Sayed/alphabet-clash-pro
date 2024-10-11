05007c7cc68cd7b5b6cb20455783463f98311134
Mohammod-Sayed <mohammodsayed536@gmail.com>
2024-10-11 13:24:34 -0400

    new commit

--- js/alphabetClash.js
+++ js/alphabetClash.js
@@ -2,8 +2,8 @@ function continueGame(){
     const alphabet = getRendomAlphabet()
     const currentAlphabet = document.getElementById('current_alphabet')
     currentAlphabet.innerText = alphabet
+    setBackgroundColorByID(alphabet)
 }
-
 function play(){
     hideElementByID('home_section')
     showElementByID('play_ground')
