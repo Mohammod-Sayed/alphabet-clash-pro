019deb215fdefb09e16ff0faf60aab261532e8b1
Mohammod-Sayed <mohammodsayed536@gmail.com>
2024-10-12 02:59:20 -0400

    mew commit

--- js/alphabetClash.js
+++ js/alphabetClash.js
@@ -9,3 +9,23 @@ function play(){
     showElementByID('play_ground')
     continueGame()
 }
+
+document.addEventListener('keyup', (event)=>{
+    const playerPressed = event.key
+    const currentAlphabet = document.getElementById('current_alphabet').innerText
+    const lowerCaseAlphabet = currentAlphabet.toLowerCase()
+    if(playerPressed === lowerCaseAlphabet){
+        removeBackgroundColorByID(lowerCaseAlphabet)
+        continueGame()
+        const currentScore = document.getElementById('current_score')
+        const currentScoreText = parseInt(currentScore.innerText)
+        const newScore = currentScoreText+1
+        currentScore.innerText = newScore
+    }
+    else{
+        const currentLife = document.getElementById('current_life')
+        const currentLifeText = parseInt(currentLife.innerText)
+        const newLife = currentLifeText-1
+        currentLife.innerText = newLife 
+    }
+})
--- js/utility.js
+++ js/utility.js
@@ -1,11 +1,23 @@
 function hideElementByID(elementID){
-    const element = document.getElementById(elementID)
-    element.classList.add('hidden')
+    const hideElement = document.getElementById(elementID)
+    hideElement.classList.add('hidden')
 }
+
 function showElementByID(elementID){
-    const element = document.getElementById(elementID)
-    element.classList.remove('hidden')
+    const showElement = document.getElementById(elementID)
+    showElement.classList.remove('hidden')
 }
+
+function setBackgroundColorByID(elementID){
+    const setElement = document.getElementById(elementID)
+    setElement.classList.add('bg-[#FFA500]')
+}
+
+function removeBackgroundColorByID(elementID){
+    const removeElement = document.getElementById(elementID)
+    removeElement.classList.remove('bg-[#FFA500]')
+}
+
 function getRendomAlphabet(){
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz'
     const alphabets = alphabetString.split('')
@@ -14,8 +26,4 @@ function getRendomAlphabet(){
     const index = Math.round(randomNumber)
     const alphabet = alphabets[index]
     return alphabet
-}
-function setBackgroundColorByID(elementID){
-    const element = document.getElementById(elementID)
-    element.classList.add('bg-[#FFA500]')
 }
